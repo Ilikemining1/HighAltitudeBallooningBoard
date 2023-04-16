ecc5b988f6153aa808f0642b601418b64708ec0e
IlikeTech <spbach1234@gmail.com>
2023-04-16 17:46:32 -0400

    Fixed dynamics issues

--- HighAltitudeBallooning/HighAltitudeBallooning.ino
+++ HighAltitudeBallooning/HighAltitudeBallooning.ino
@@ -7,8 +7,7 @@
 #include <INA219.h>
 #include <ICM20948_WE.h>
 
-#include <SD.h>  // Telemetry and Datalogging
-#include <RadioLib.h>
+#include <SD.h>
 
 INA219 sysBus;
 Adafruit_BME680 bme;
@@ -17,10 +16,6 @@ ICM20948_WE imu = ICM20948_WE(0x68);
 
 int fileNumber = 1;
 char fileName[25];
-
-//SX1276 vhf = new Module(8, 14, 6, 15);
-//SX1276 uhf = new Module(9, 20, 7, 21);
-
 
 void setup() {
   Serial.begin(115200);  // Initialize USB UART for debug
@@ -46,15 +41,16 @@ void setup() {
     gnss.setSerialRate(115200);
     watchdog_reboot(0, 0, 0);  // Reset to apply baud rate.
   }
-  gnss.setUART1Output(COM_TYPE_UBX);
 
-  if (sysBus.begin()) {  // INA219 Initialization
-    Serial.println("INA219 initialization failed!");
+  gnss.setUART1Output(COM_TYPE_UBX);
+  gnss.setDynamicModel(DYN_MODEL_AIRBORNE2g);
+  if (gnss.getDynamicModel() != DYN_MODEL_AIRBORNE2g) {
+    Serial.println("Dynamics model could not be updated!");
     while (1);
   }
 
-  if (!imu.init()) {  // ICM20948 Initialization
-    Serial.println("ICM20948 initialization failed!");
+  if (sysBus.begin()) {  // INA219 Initialization
+    Serial.println("INA219 initialization failed!");
     while (1);
   }
 
@@ -72,6 +68,11 @@ void setup() {
   bme.setHumidityOversampling(BME680_OS_2X);
   bme.setPressureOversampling(BME680_OS_4X);
 
+  if (!imu.init()) {  // ICM20948 Initialization
+    Serial.println("ICM20948 initialization failed!");
+    while (1);
+  }
+
   Serial.println("IMU Auto-Level, do not disturb!");
   delay(1000);
   imu.autoOffsets();
@@ -85,9 +86,10 @@ void setup() {
   imu.setGyrDLPF(ICM20948_DLPF_6);
 
   pinMode(28, OUTPUT);
-
   pinMode(2, OUTPUT);
   pinMode(3, OUTPUT);
+
+  pinMode(29, INPUT_PULLUP);
 
   while (1) {
     snprintf(fileName, 25, "data%u.csv", fileNumber);
@@ -134,26 +136,7 @@ void loop() {
   xyzFloat magVal = imu.getMagValues();
   xyzFloat gyroVal = imu.getGyrValues(); 
 
-/*
-  Serial.printf("Current Temperature: %lf degrees C\n", bme.temperature); // Print BME Data
-  Serial.printf("Current Pressure: %lf hPa\n", bme.pressure / 100.0);
-  Serial.printf("Current Humidity: %lf %\n", bme.humidity);
-
-  Serial.printf("Current Bus Voltage: %lf V\n", sysBus.busVoltage());
-  Serial.printf("Current Bus Current: %lf mA\n", sysBus.shuntCurrent() * 1000.0);
-
-  Serial.printf("Current GNSS Fix Type: %d Current SIV: %d\n", gnss.getFixType(), gnss.getSIV());
-  Serial.printf("Current Latitude: %lf degrees\n", (double) gnss.getLatitude() / 10000000.0);
-  Serial.printf("Current Longitude: %lf degrees\n", (double) gnss.getLongitude() / 10000000.0);
-  Serial.printf("Current GNSS Altitude: %lf m\n", (double) gnss.getAltitude() / 1000.0);
-
-  Serial.printf("Current Acceleration x:%lf, y:%lf, z:%lf g\n", gVal.x, gVal.y, gVal.z);
-  Serial.printf("Current Mag Values x:%lf, y:%lf, z:%lf\n", magVal.x, magVal.y, magVal.z);
-  Serial.printf("Current Gyro Values x:%lf, y:%lf, z:%lf\n", gyroVal.x, gyroVal.y, gyroVal.z);
-
-  Serial.printf("\n\n\n\n\n"); */
-
-  snprintf(line, 300, "%u/%u/%u,%u:%u:%u,%lf,%lf,%lf,%lf,%lf,%lf,%u,%u,%lf,%lf,%lf,%lf,%lf,%lf,%lf,%lf,%lf,%lf,%lf \n", gnss.getYear(), gnss.getMonth(), gnss.getDay(), gnss.getHour(), gnss.getMinute(), gnss.getSecond(), bme.temperature, bme.pressure / 100.0, bme.humidity, gnss.getLatitude() / 10000000.0, gnss.getLongitude() / 10000000.0, gnss.getAltitude() / 1000.0, gnss.getFixType(), gnss.getSIV(), gVal.x, gVal.z, gVal.z, magVal.x, magVal.y, magVal.z, gyroVal.x, gyroVal.y, gyroVal.z, sysBus.busVoltage(), sysBus.shuntCurrent() * 1000.0);
+  snprintf(line, 300, "%u/%u/%u,%u:%u:%u,%lf,%lf,%lf,%lf,%lf,%lf,%u,%u,%lf,%lf,%lf,%lf,%lf,%lf,%lf,%lf,%lf,%lf,%lf \n", gnss.getYear(), gnss.getMonth(), gnss.getDay(), gnss.getHour(), gnss.getMinute(), gnss.getSecond(), bme.temperature, bme.pressure / 100.0, bme.humidity, gnss.getLatitude() / 10000000.0, gnss.getLongitude() / 10000000.0, gnss.getAltitude() / 1000.0, gnss.getFixType(), gnss.getSIV(), gVal.x, gVal.y, gVal.z, magVal.x, magVal.y, magVal.z, gyroVal.x, gyroVal.y, gyroVal.z, sysBus.busVoltage(), sysBus.shuntCurrent() * 1000.0);
 
   File data = SD.open(fileName, "a+");
 
@@ -165,6 +148,18 @@ void loop() {
   data.print(line);
 
   data.close();
+
+  if (digitalRead(29) == LOW) {
+    data.close();
+    digitalWrite(2, LOW);
+    digitalWrite(3, LOW);
+
+    while(true) {
+      digitalWrite(28, !digitalRead(28));
+      delay(100); 
+    }
+
+  }
 
   digitalWrite(28, !digitalRead(28));
   delay(50);
